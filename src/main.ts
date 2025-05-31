d5f8665303c55931085a4c664eabb23ab760eba5
NastyaSvich <nastya_drakosha@bk.ru>
2025-05-31 23:41:26 +0300

    feat: implement swagger and env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const swaggerPath = resolve(__dirname, '..', 'doc', 'api.yaml');
+  const swaggerFile = await readFile(swaggerPath, 'utf-8');
+  SwaggerModule.setup('doc', app, parse(swaggerFile));
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT') || 4000;
+  await app.listen(PORT);
+  console.log(`App has been started on the port ${PORT}`);
 }
 bootstrap();
